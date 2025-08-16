77eaef69b25111ff2319453ca5e2003ad66880d4
Jigme Nidup <jigmenidup8102003@gmail.com>
2025-08-16 12:04:52 +0600

    updated arduino code

--- firmware/Smart_Garden/Smart_Garden.ino
+++ firmware/Smart_Garden/Smart_Garden.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
@@ -9,6 +9,7 @@
 #define SOIL_MOISTURE_PIN 34
 #define LDR_PIN 35
 #define RAIN_SENSOR_PIN 33
+#define POWER_PIN 19
 #define DHT_PIN 25
 #define LED_PIN 26
 
@@ -32,8 +33,44 @@ const char* mqtt_username = "smart_garden_sensor";
 const char* mqtt_password = "Smart@pass2025";
 const char* mqtt_topic = "smart-garden/sensors";
 
-WiFiClient espClient;
+WiFiClientSecure espClient;
 PubSubClient client(espClient);
+
+
+// HiveMQ Cloud Let's Encrypt CA certificate
+static const char* root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
 
 // Variables for sensor readings
 float temperature = 0;
@@ -52,6 +89,9 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW); // Start with LED off
   
+  //Initialize rain sensor power pin
+  pinMode(POWER_PIN, OUTPUT);
+
   // Initialize OLED display
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -68,7 +108,12 @@ void setup() {
   setupWiFi();
   
   // Set up MQTT
+  
+  // espClient.setInsecure();
+  espClient.setCACert(root_ca); //enable ssl
+
   client.setServer(mqtt_server, mqtt_port);
+  analogSetAttenuation(ADC_11db);
 }
 
 void loop() {
@@ -144,11 +189,15 @@ void readSensors() {
   // Read analog sensors (12-bit ADC)
   soilMoisture = analogRead(SOIL_MOISTURE_PIN);
   lightIntensity = analogRead(LDR_PIN);
+
+  digitalWrite(POWER_PIN, HIGH);  // turn the rain sensor's power  ON
+  delay(10);                      // wait 10 milliseconds
   rainValue = analogRead(RAIN_SENSOR_PIN);
+  digitalWrite(POWER_PIN, LOW);  // turn the rain sensor's power OFF
   
   // Convert analog readings to more meaningful values
   soilMoisture = map(soilMoisture, 0, 4095, 0, 100); // Convert to percentage
-  lightIntensity = map(lightIntensity, 0, 4095, 0, 1000); // Convert to lux approximation
+  lightIntensity = map(abs(lightIntensity-4095), 0, 4095, 0, 1000); // Convert to lux approximation
   rainValue = map(rainValue, 0, 4095, 0, 10); // Convert to mm approximation
   
   // Check if any reads failed
